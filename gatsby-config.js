6a7817d6c7b470579d22cc84d08cfe34f9c143ea
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2020-05-07 00:19:24 +0300

    deleted initialized values

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `TITLE`,
+    description: `DESCRIPTION.`,
+    author: `@mustafaberat`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `photos`,
+        path: `${__dirname}/src/photos`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -24,7 +24,6 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
