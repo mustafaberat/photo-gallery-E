319648b0fa005327de33005da4aea49d1b156b10
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2020-05-07 01:06:22 +0300

    image component done

--- src/components/image.js
+++ src/components/image.js
@@ -2,31 +2,32 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 
-/*
- * This component is built using `gatsby-image` to automatically serve optimized
- * images with lazy loading and reduced file sizes. The image is loaded using a
- * `useStaticQuery`, which allows us to load the image from directly within this
- * component, rather than having to pass the image data down from pages.
- *
- * For more information, see the docs:
- * - `gatsby-image`: https://gatsby.dev/gatsby-image
- * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
- */
 
 const Image = () => {
   const data = useStaticQuery(graphql`
-    query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 300) {
-            ...GatsbyImageSharpFluid
+     query ImageQuery{
+      allFile(filter:{absolutePath:{regex: "//photos//"}}) {
+        edges{
+          node{
+            id
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
           }
         }
       }
     }
   `)
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+  return (
+    <section className="images">
+      {data.allFile.edges.map((edge) => {
+        return <Img key={edge.node.id} fluid={edge.node.childImageSharp.fluid} />
+      })}
+    </section>
+  )
 }
 
 export default Image
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,19 @@
 import React from "react"
 
-const IndexPage = () => (
-  <div>
-    <h1>Hello World</h1>
-  </div>
-)
+import Image from "../components/image.js"
+
+import './main.css'
+
+const IndexPage = () => {
+  return (
+    <div>
+      <div className="titleContainer">
+        <h1 className="titleContainer-title">Sizden Gelen Güzellikler</h1>
+        <p className="titleContainer-description">Fotoğrafını bu sitede paylaşabilirsin</p>
+      </div>
+      <Image />
+    </div>
+  )
+}
 
 export default IndexPage
