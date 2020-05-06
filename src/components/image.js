c842f449fca3a81ddf7cadfbd7cdd8cafb0a930f
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2020-05-07 01:14:36 +0300

    header component and image component optimized

--- src/components/image.js
+++ src/components/image.js
@@ -2,6 +2,7 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 
+import '../pages/main.css'
 
 const Image = () => {
   const data = useStaticQuery(graphql`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,13 @@
 import React from "react"
 
 import Image from "../components/image.js"
+import Header from "../components/header.js"
 
-import './main.css'
 
 const IndexPage = () => {
   return (
     <div>
-      <div className="titleContainer">
-        <h1 className="titleContainer-title">Sizden Gelen Güzellikler</h1>
-        <p className="titleContainer-description">Fotoğrafını bu sitede paylaşabilirsin</p>
-      </div>
+      <Header className="titleContainer" siteTitle="Sizden Gelen Güzellikler" siteDescription="Fotoğrafını bu sitede paylaşabilirsin" />
       <Image />
     </div>
   )
